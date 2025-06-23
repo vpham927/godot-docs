332c2fe4f436a3449cdd414d713ebe5499bb4188
Viet Pham <viet@Viets-MacBook-Pro-2.local>
2025-06-23 19:40:38 +0100

    final task

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,6 +8,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 	
 func new_game():
 	score = 0
@@ -16,6 +18,7 @@ func new_game():
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
 	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 	
 func _ready() -> void:
 	pass
